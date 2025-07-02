705012c3b360d4a3a119b63599ba22b698a9c75d
Mattoctet <matthias.breton@free.fr>
2025-07-03 01:29:41 +0200

    v.6.0.1

--- extension.py
+++ extension.py
@@ -1,21 +1,17 @@
 import os
 
-# Dossier courant
 racine = os.getcwd()
 
-# Parcours récursif des fichiers
 for dossier_racine, sous_dossiers, fichiers in os.walk(racine):
     for fichier in fichiers:
-        # Exclure les fichiers AppleDouble et tout fichier qui ne finit pas par ".html"
-        if fichier.startswith("._") or not fichier.lower().endswith(".jsn"):
-            continue
+        # Ne traiter que les fichiers .shtm (sans majuscule)
+        if fichier.lower().endswith(".html"):
+            ancien_chemin = os.path.join(dossier_racine, fichier)
+            nouveau_nom = fichier[:-5] + ".htm"  # Remplace .shtm par .html
+            nouveau_chemin = os.path.join(dossier_racine, nouveau_nom)
 
-        ancien_chemin = os.path.join(dossier_racine, fichier)
-        nouveau_nom = fichier[:-5] + ".jsn"
-        nouveau_chemin = os.path.join(dossier_racine, nouveau_nom)
-
-        try:
-            os.rename(ancien_chemin, nouveau_chemin)
-            print(f"[OK] {ancien_chemin} → {nouveau_chemin}")
-        except Exception as e:
-            print(f"[ERREUR] {ancien_chemin} : {e}")
+            try:
+                os.rename(ancien_chemin, nouveau_chemin)
+                print(f"[OK] {ancien_chemin} → {nouveau_chemin}")
+            except Exception as e:
+                print(f"[ERREUR] {ancien_chemin} : {e}")
